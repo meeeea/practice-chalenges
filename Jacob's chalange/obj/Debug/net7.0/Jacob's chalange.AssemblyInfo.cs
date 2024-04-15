8b7a6997165d25a9e053e3cf266396ca75a3077b
Justin Hutchens <Justinh22007@gmail.com>
2024-04-15 09:19:20 -0500

    just adding it

--- Jacob's chalange/obj/Debug/net7.0/Jacob's chalange.AssemblyInfo.cs
+++ Jacob's chalange/obj/Debug/net7.0/Jacob's chalange.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jacob\'s chalange")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d1d1c09f894b98fb213718c09ee50e5a686934e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jacob\'s chalange")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jacob\'s chalange")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
